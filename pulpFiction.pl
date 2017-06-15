9765a68068f0c9b2ab66a808c96bfb2ebc3f9351
Unknown <franciscodumont1@gmail.com>
2017-06-15 18:51:29 -0300

    Nuevas parejas y trabajadores
    Bernardo es pareja de Bianca y de Charo.
    
    Bernardo trabaja para cualquiera que trabaje para marsellus (salvo para jules)
    George trabaja para todos los que salen con Bernardo

--- pulpFiction.pl
+++ pulpFiction.pl
@@ -1,10 +1,17 @@
 %pareja(Persona, Persona)
 pareja(marsellus,mia).
 pareja(pumkin,honeyBunny).
+pareja(bernardo,bianca).
+pareja(bernardo,charo).
 %trabajaPara(Empleador, Empleado)
 trabajaPara(marsellus,vincent).
 trabajaPara(marsellus,jules).
 trabajaPara(marsellus,winston).
+trabajaPara(Empleador,bernardo):-
+	trabajaPara(marsellus,Empleador),
+	Empleador\=jules.
+trabajaPara(Empleador,george):-
+	saleCon(bernardo,Empleador).
 
 %saleCon/2: relaciona dos personas que están saliendo porque son pareja, independientemente de cómo esté definido en el predicado pareja/2.
 saleCon(Persona,OtraPersona):-
