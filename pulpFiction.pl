425dbb4f2d391f80bdf6886599df66a035bf7781
mackhood <germanjugo@hotmail.com>
2017-06-15 22:03:03 -0300

    Create pulpFiction.pl

--- pulpFiction.pl
+++ pulpFiction.pl
@@ -33,3 +33,58 @@ acataOrden(Superior,Empleado):-
 	trabajaPara(Empleador,Empleado),
 	acataOrden(Superior,Empleador).
 	
+	
+	
+personaje(pumkin,     ladron([estacionesDeServicio, licorerias])).
+personaje(honeyBunny, ladron([licorerias, estacionesDeServicio])).
+personaje(vincent,    mafioso(maton)).
+personaje(jules,      mafioso(maton)).
+personaje(marsellus,  mafioso(capo)).
+personaje(winston,    mafioso(resuelveProblemas)).
+personaje(mia,        actriz([foxForceFive])).
+personaje(butch,      boxeador).
+personaje(bernardo,   mafioso(cerebro)).
+personaje(bianca,     actriz([elPadrino1])).
+personaje(elVendedor, vender([humo, iphone])).
+personaje(jimmie,     vender([auto])).
+ 
+También tenemos información de los encargos que le hacen los jefes a sus empleados, codificada en la base de la siguiente forma: 
+% encargo(Solicitante, Encargado, Tarea). 
+% las tareas pueden ser cuidar(Protegido), ayudar(Ayudado), buscar(Buscado, Lugar)
+encargo(marsellus, vincent,   cuidar(mia)).
+encargo(vincent,  elVendedor, cuidar(mia)).
+encargo(marsellus, winston, ayudar(jules)).
+encargo(marsellus, winston, ayudar(vincent)).
+encargo(marsellus, vincent, buscar(butch, losAngeles)).
+encargo(bernardo, vincent, buscar(jules, fuerteApache)).
+encargo(bernardo, winston, buscar(jules, sanMartin)).
+encargo(bernardo, winston, buscar(jules, lugano)).
+ 
+Por último contamos con la información de quién es amigo de quién:
+amigo(vincent, jules).
+amigo(jules, jimmie).
+amigo(vincent, elVendedor).
+
+
+
+nivelRespeto(Personaje,Nivel):-personaje(Personaje,Ocupacion),cantidadNivel(Ocupacion,Nivel).
+cantidadNivel(mafioso(resuelveProblemas),10).
+cantidadNivel(mafioso(capo),20).
+nivelRespeto(Personaje,Nivel):-personaje(Personaje,actriz(ListaPeliculas)),legnth(ListaPeliculas,Cantidad),Cantidad/10 is Nivel.
+nivelRespeto(_,0).
+
+
+
+respetable(Personaje,Nivel):- nivelRespeto(Personaje,Nivel),Nivel>9.
+
+
+respetabilidad(Respetables , NoRespetables):-findall(Personaje,respetable(Personaje,Nivel),PersonajesRespetables),
+					length(PersonajesRespetables,Respetanles),
+					findall(Personaje,not(respetable(Personaje,Nivel)),PersonajesNoRespetables),
+					length(PersonajesNoRespetables,NoRespetables).
+
+
+cantidadEncargos(Personaje,Cantidad):-findall(Personaje,encargo(Personaje,_,_),CantidadEncargos),length(CantidadEncargos,Cantidad).
+masAtareado(Personaje):- forall	(personje(Personaje,_),			
+
+
